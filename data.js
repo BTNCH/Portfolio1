915c5b833f0bce6b5290bf5e1c0c61bf0abb45a0
Tanachart <614259044@webmail.npru.ac.th>
2020-10-05 14:00:14 +0700

    finish commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const data = {
+    "name":"Tanachart Chaonafai",
+    "Primaryschool": "Amnuaiwit Nakhon Pathom School",
+    "aboutme":{
+        "Fullname":"Tanachart Chaonafai",
+        "nickname":"Boat",
+        "age": "20",
+        "Phone":"(+66)090 445 0984",
+        "Email":"614259044@webmail.npru.ac.th",
+        "Website":"http://facebook/Tanachad.com",
+        "Address":" 29/2 Moo 6, Wang Taku Subdistrict, Nakhon Pathom Province 73000",
+
+    "Primaryschool": "Amnuaiwit Nakhon Pathom School",
+    "Highschool" : "Phrapathomwitthayalai school",
+    "present" : "Nakhon Pathom Rajabhat University",
+    "work":"Studen in Nakhon Pathom Rajabhat University",
+        "skill" : "Word   Power Point  Photoshop>"
+    
+}
+
+}
+module.exports = data;
